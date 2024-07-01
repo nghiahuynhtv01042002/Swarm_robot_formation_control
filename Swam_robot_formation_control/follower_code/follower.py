f9d2851273a899ca358bb0a79a9643e943de9469
nghiahuynhtv01042002 <nghiahuynhtv01042002@gmail.com>
2024-07-01 21:44:03 +0700

    first commit

---
+++ Swam_robot_formation_control/follower_code/follower.py
@@ -0,0 +1,189 @@
+import numpy as np
+import serial
+import socket
+import threading
+import time
+# Instant parameter
+serial_port = '/dev/ttyUSB0'
+baud_rate = 115200  # Set your baud rate
+
+cmd = "RUN"
+L = 0.4
+vmax = 0.17
+
+global leader
+global follower
+global v_l,v_r
+# Semaphores for synchronization
+leader_semaphore = threading.Semaphore()
+follower_semaphore = threading.Semaphore()
+
+# Function code
+def limit_velocity(v_r,v_l):
+    if v_r > vmax: 
+        v_r = vmax
+    elif v_r < -vmax:
+        v_r = -vmax
+
+    if v_l > vmax : 
+        v_l = vmax
+    elif v_l < -vmax:
+        v_l = -vmax
+    return v_r,v_l
+def decoder_frame_data(data_received):
+    split_index_CMD_start = data_received.index(':')
+    split_index_CMD_stop = data_received.index('#', split_index_CMD_start + 1)
+
+    split_index_X_start = data_received.index(':', split_index_CMD_stop + 1)
+    split_index_X_stop = data_received.index('#', split_index_X_start + 1)
+
+    split_index_Y_start = data_received.index(':', split_index_X_stop + 1)
+    split_index_Y_stop = data_received.index('#', split_index_Y_start + 1)
+
+    split_index_Theta_start = data_received.index(':', split_index_Y_stop + 1)
+    split_index_Theta_stop = data_received.index('#', split_index_Theta_start + 1)
+
+    cmd = data_received[split_index_CMD_start + 1:split_index_CMD_stop]
+    X = float(data_received[split_index_X_start + 1:split_index_X_stop])
+    Y = float(data_received[split_index_Y_start + 1:split_index_Y_stop])
+    Theta = float(data_received[split_index_Theta_start + 1:split_index_Theta_stop])
+    return cmd, X, Y, Theta
+
+# PID class
+class PID:
+    def __init__(self, Kp, Ki, Kd):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.prev_error = 0
+        self.integral = 0
+
+    def update(self, error):
+        self.integral += error 
+        derivative = (error - self.prev_error) 
+        output = self.Kp * error + self.Ki * self.integral + self.Kd * derivative
+        self.prev_error = error
+        return output
+
+# Robot class
+class Robot:
+    def __init__(self, cmd, x, y, theta):
+        self.cmd = cmd
+        self.x = x
+        self.y = y
+        self.theta = theta
+
+# Initialization
+leader = Robot("STP", 0, 0, 0)
+follower = Robot("RUN", -L, 0, np.deg2rad(0))
+v_r = 0
+v_l = 0
+# Initialize PID controllers
+pid_vx = PID(15, 0.01, 0.01)
+pid_vy = PID(15, 0.01 ,0.01)
+
+# Create socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('192.168.169.84', 12345))
+
+# Create thread to read data from socket server
+def read_leader_odom():
+    global leader
+    while True:
+        data_received_socket = client_socket.recv(1024).decode().strip()
+        leader_semaphore.acquire()
+        leader.cmd, leader.x, leader.y, leader.theta = decoder_frame_data(data_received_socket)
+        leader_semaphore.release()
+
+thread_read_leader = threading.Thread(target=read_leader_odom)
+thread_read_leader.start()
+
+# Create and open serial port 
+ser = serial.Serial(serial_port, baud_rate)
+
+# Create thread to read data from MCU
+def read_follower_odom():
+    global follower
+    while True:
+        data_received_mcu = ser.readline().decode('utf-8').strip()
+        follower_semaphore.acquire()
+        follower.cmd, follower.x, follower.y, follower.theta = decoder_frame_data(data_received_mcu)
+        follower_semaphore.release()
+
+thread_read_follower = threading.Thread(target=read_follower_odom)
+thread_read_follower.start()
+
+
+def send_data_to_MCU():
+    global v_r,v_l
+    while True:
+        data_send2MCU = "!cmd:{}#v_r:{:0.2f}#v_l:{:0.2f}#\n".format("RUN", v_r, v_l)
+        ser.write(data_send2MCU.encode())
+        time.sleep(0.1)
+
+thread_send_dataMCU = threading.Thread(target=send_data_to_MCU)
+thread_send_dataMCU.start()
+
+try: 
+    # Infinite loop
+    while True:
+        # global leader,follower,v_r,v_l
+        # Print data
+
+        # leader_semaphore.acquire()
+        # data_leader = "leader: x:{}, y:{}, theta:{}\n".format(leader.x, leader.y, leader.theta)
+        # leader_semaphore.release()
+        # print(data_leader)
+
+        follower_semaphore.acquire()
+        data_follower = "follower: x:{}, y:{}, theta:{}\n".format(follower.x, follower.y, follower.theta)
+        follower_semaphore.release()
+        print(data_follower)
+
+        # Calculate desired point 
+        leader_semaphore.acquire()
+        x_d = leader.x - L * np.cos(np.deg2rad(leader.theta))
+        y_d = leader.y - L * np.sin(np.deg2rad(leader.theta))
+        leader_semaphore.release()
+        # Calculate error ex and ey
+        follower_semaphore.acquire()
+        x_error = x_d - follower.x
+        y_error = y_d - follower.y
+        follower_semaphore.release()
+        # Calculate PID output vx and vy 
+        vx = pid_vx.update(x_error)
+        vy = pid_vy.update(y_error)
+        # Calculate v and w (omega)
+        follower_semaphore.acquire()
+        v = vx * np.cos(np.deg2rad(follower.theta)) + vy * np.sin(np.deg2rad(follower.theta))
+        w =   1 * (-vx * np.sin(np.deg2rad(follower.theta)) + vy * np.cos(np.deg2rad(follower.theta))) / 0.0325
+        # w = (-vx * np.sin(np.deg2rad(follower.theta)) + vy * np.cos(np.deg2rad(follower.theta))) 
+        # print("vx:{:0.2f},vy:{:0.2f},v:{:0.2f},w:{:0.2f}".format(vx,vy,v,w))
+        follower_semaphore.release()
+
+        # Calculate vr and vl
+        v_r = v + (w * 0.2 / 2)
+        v_l = v - (w * 0.2 / 2)
+        v_r, v_l = limit_velocity(v_r, v_l)
+
+        print("velocity: !cmd:{}#v_r:{}#v_l:{}".format(follower.cmd, v_r, v_l))
+        # Stop condition
+        leader_semaphore.acquire()
+        follower_semaphore.acquire()
+        # if np.sqrt((leader.x - follower.x)**2 + (leader.y - follower.y)**2) <= 0.4:
+        #     v_r = v_l = 0
+        #     follower.cmd = "STP"
+        if np.sqrt((x_d - follower.x)**2 + (y_d - follower.y)**2) <= 0.03:
+            v_r = v_l = 0
+
+        leader_semaphore.release()
+        follower_semaphore.release()
+        time.sleep(0.1)
+
+
+except KeyboardInterrupt:
+    client_socket.close()
+    ser.close()
+    thread_read_leader.join()
+    thread_read_follower.join()
+    send_data_to_MCU.join()
---
+++ Swam_robot_formation_control/leader_code/leader.py
@@ -0,0 +1,372 @@
+import serial
+import time
+import math
+import obstacles as ob
+import numpy as np
+import socket
+
+
+
+def decoder_frame_data( data_received):
+
+    split_index_CMD_start = data_received.index(':')
+    split_index_CMD_stop = data_received.index('#',split_index_CMD_start + 1)
+
+    split_index_X_start = data_received.index(':',split_index_CMD_stop+1)
+    split_index_X_stop = data_received.index('#',split_index_X_start+1)
+
+    split_index_Y_start = data_received.index(':',split_index_X_stop+1)
+    split_index_Y_stop = data_received.index('#',split_index_Y_start+1)
+
+    split_index_Theta_start = data_received.index(':',split_index_Y_stop+1)
+    split_index_Theta_stop = data_received.index('#',split_index_Theta_start+1)
+
+    cmd = data_received[split_index_CMD_start+1:split_index_CMD_stop]
+    X = float(data_received[split_index_X_start+1:split_index_X_stop])
+    Y = float(data_received[split_index_Y_start+1:split_index_Y_stop])
+    Theta = float(data_received[split_index_Theta_start+1:split_index_Theta_stop])   
+    return cmd,X,Y,Theta
+
+def decoder_frame_vel(data_received):
+    split_index_CMD_start = data_received.index(':')
+    split_index_CMD_stop = data_received.index('#',split_index_CMD_start + 1)
+    split_index_vr_start = data_received.index(':',split_index_CMD_stop+1)
+    split_index_vr_stop = data_received.index('#',split_index_vr_start+1)
+    split_index_vl_start = data_received.index(':',split_index_vr_stop+1)
+    split_index_vl_stop = data_received.index('#',split_index_vl_start+1)
+
+    cmd = data_received[split_index_CMD_start+1:split_index_CMD_stop]
+    vr = float(data_received[split_index_vr_start+1 : split_index_vr_stop])
+    vl = float(data_received[split_index_vl_start+1 : split_index_vl_stop])
+    return cmd ,vr, vl 
+
+def decoder_frame_enc(data_received):
+    split_index_CMD_start = data_received.index(':')
+    split_index_CMD_stop = data_received.index('#',split_index_CMD_start + 1)
+    split_index_enc_r_start = data_received.index(':',split_index_CMD_stop+1)
+    split_index_enc_r_stop = data_received.index('#',split_index_enc_r_start+1)
+    split_index_enc_l_start = data_received.index(':',split_index_enc_r_stop+1)
+    split_index_enc_l_stop = data_received.index('#',split_index_enc_l_start+1)
+
+    cmd = data_received[split_index_CMD_start+1:split_index_CMD_stop]
+    enc_r = float(data_received[split_index_enc_r_start+1 : split_index_enc_r_stop])
+    enc_l = float(data_received[split_index_enc_l_start+1 : split_index_enc_l_stop])
+    return cmd ,enc_r, enc_l 
+
+# def send_point(x, y, theta):
+#     sending_data = "!cmd:RUN#x:{}#y:{}#theta:{}#\n".format(x, y, theta)
+#     ser.write(sending_data.encode())
+#     print(sending_data)
+
+# def send_destiny(*points):
+#     if not points:
+#         return
+#     temp_point = points.pop(0);
+#     send_point(temp_point)
+
+def send_point(points,cmd):
+    # global cmd
+    if not points:
+        return 
+    if cmd == "STP":
+        # time.sleep(10)
+        temp = points.pop(0)
+
+        sending_data = "!cmd:RUN#x:{}#y:{}#theta:0.00#\n".format(temp[0],temp[1])
+        ser.write(sending_data.encode())
+        print("data send to MCU: "+sending_data)
+        print("################################################ points was sent ##############################33#############################3\n")
+        # cmd = "RUN"
+
+#PSO here
+
+def random_initialization(swarm_size):
+    """
+    Random initializations of PSO particles location and velocity.
+
+    Parameters:
+        swarm_size (int): How many particles are present.
+
+    Returns:
+        List[List[float, float]]: List of starting coordinates of the particles, total number = swarm_size * DIM * 2.
+        List[float, float]: List of starting velocity of the particles, total number = swarm_size * DIM * 2.
+        List[floats]: Each particle best loss function result.
+        List[List[float, float]]: List of best velocity of the particles (there's only the starting in the initialization).
+        float: Best loss function result.
+        List[float, float]: The best location.
+    """
+
+    # set the location and velocity of the particle's
+    # particles_loc = np.random.rand(swarm_size, DIM, 2) * 20
+    particles_loc_x = np.random.rand(swarm_size,DIM)*20
+    particles_loc_y = np.random.rand(swarm_size,DIM)*10
+    particles_loc = np.stack((particles_loc_x,particles_loc_y),axis= -1)
+
+    particles_vel = np.random.rand(swarm_size, DIM, 2)
+
+    # set the initial particle best location and value
+    particles_lowest_loss = [
+        loss_function(particles_loc[i, :, :]) for i in range(0, len(particles_loc))
+    ]
+    particles_best_location = np.copy(particles_loc)
+
+    # set the initial global best location and value
+    global_lowest_loss = np.min(particles_lowest_loss)
+    global_best_location = particles_loc[np.argmin(particles_lowest_loss)].copy()
+
+    return (
+        particles_loc,
+        particles_vel,
+        particles_lowest_loss,
+        particles_best_location,
+        global_lowest_loss,
+        global_best_location,
+    )
+
+
+def loss_function(x):
+    """
+    Loss function to find the shortest path.
+
+    Parameters:
+        x (List[Point]): List of points representing the path of the agent.
+
+    Returns:
+        int: The loss function result
+    """
+
+    z = (x[0, 0] - START.x) ** 2 + (x[0, 1] - START.y) ** 2
+    for i in range(DIM - 1):
+        z = z + ((x[i, 0] - x[i + 1, 0]) ** 2 + (x[i, 1] - x[i + 1, 1]) ** 2)
+    z = z + (x[DIM - 1, 0] - END.x) ** 2 + (x[DIM - 1, 1] - END.y) ** 2
+    return math.sqrt(z)
+
+
+
+# def loss_function(x):
+#     """
+#     Loss function to find the shortest path including the maximum distance between consecutive points.
+
+#     Parameters:
+#         x (List[Point]): List of points representing the path of the agent.
+
+#     Returns:
+#         float: The loss function result
+#     """
+
+#     # Initialize the loss with the distance from the start to the first point
+#     z = (x[0, 0] - START.x) ** 2 + (x[0, 1] - START.y) ** 2
+#     distance_Start2End = math.sqrt((START.x - END.x)**2 + (END.y - START.y)**2)
+#     # Initialize the variable to track the maximum distance
+#     max_distance = 0
+#     penaty_distance = 0
+#     # Calculate the sum of squared distances between consecutive points and track the maximum distance
+#     for i in range(DIM - 1):
+#         distance = (x[i, 0] - x[i + 1, 0]) ** 2 + (x[i, 1] - x[i + 1, 1]) ** 2
+#         z += distance
+#         if distance > (distance_Start2End)/(DIM+1):
+#             # max_distance = distance
+#             penaty_distance +=1
+        
+    
+#     # Add the distance from the last point to the end
+#     z += (x[DIM - 1, 0] - END.x) ** 2 + (x[DIM - 1, 1] - END.y) ** 2
+#     z += 1*z
+#     # Add the maximum distance to the loss
+#     z += penaty_distance*1
+    
+#     return math.sqrt(z)
+
+
+
+def is_valid(circles, p):
+    """
+    Check if the point p is valid (i.e. doesn't go into obstacles).
+
+    Parameters:
+        circles (List[Obstacle_Circle]): The list of obstacles.
+        p (Point): The point to check.
+
+    Returns:
+        boolean: wheter the point is valid
+    """
+    to_add = ob.Point(0, 0)
+    point_p = ob.Point(p[0], p[1])
+    for i in range(len(circles)):
+        if circles[i].inside_circle(point_p):
+            to_add = ob.Point(
+                circles[i].how_to_exit_x(point_p.x) + to_add.x,
+                circles[i].how_to_exit_y(point_p.y) + to_add.y,
+            )
+    return to_add
+def particle_swarm_optimization(
+    max_iterations, swarm_size, max_vel, step_size, inertia, c1, c2, circles
+):
+    """
+    Implementation of the Particle Swarm Optimization algorithm randomly.
+
+    Parameters:
+        max_iterations (int): The maximum number of iterations.
+        swarm_size (int): The number of particles.
+        max_vel (int): Maximum velocity for a particle.
+        step_size (int): The step size for updating each particle (how far a particle travels before its velocity is readjusted).
+        inertia (int): The inertia of a particle.
+        c1 (int): Factor for the particles local best.
+        c2 (int): Factor for the the global best.
+        circles (Obstacle_Circle): Obstacle of circular shape.
+
+    Returns:
+        boolean: the best path
+    """
+    (
+        particles_loc,
+        particles_vel,
+        particles_lowest_loss,
+        particles_best_location,
+        global_lowest_loss,
+        global_best_location,
+    ) = random_initialization(swarm_size)
+    best_location = []
+    print_iteration = 1
+
+    for iteration_i in range(max_iterations):
+        if iteration_i % 20 == 0:
+            print("%i%%" % int(iteration_i / max_iterations * 100))
+            print_iteration += 1
+        for particle_i in range(swarm_size):
+            dim_i = 0
+            while dim_i < DIM:
+                # update the velocity vector in a given dimension
+                error_particle_best = (
+                    particles_best_location[particle_i, dim_i]
+                    - particles_loc[particle_i, dim_i]
+                )
+                error_global_best = (
+                    global_best_location[dim_i] - particles_loc[particle_i, dim_i]
+                )
+                new_vel = (
+                    inertia
+                    * min(1, (dim_i ** (0.5)) / 4)
+                    * particles_vel[particle_i, dim_i]
+                    + c1 * np.random.rand(1) * error_particle_best
+                    + c2 * np.random.rand(1) * error_global_best
+                )
+
+                # bound a particle's velocity to the maximum value
+                if new_vel[0] < -max_vel:
+                    new_vel[0] = -max_vel
+                elif new_vel[0] > max_vel:
+                    new_vel[0] = max_vel
+                if new_vel[1] < -max_vel:
+                    new_vel[1] = -max_vel
+                elif new_vel[1] > max_vel:
+                    new_vel[1] = max_vel
+
+                # update the particle location and velocity
+                particles_loc[particle_i, dim_i] = (
+                    particles_loc[particle_i, dim_i] + new_vel[:] * step_size
+                )
+                particles_vel[particle_i, dim_i] = new_vel[:]
+
+                particle_help = is_valid(circles, particles_loc[particle_i, dim_i, :])
+                particles_loc[particle_i, dim_i, 0] += particle_help.x
+                particles_loc[particle_i, dim_i, 1] += particle_help.y
+                if abs(particle_help.x) > 0.01 or abs(particle_help.y) > 0.01:
+                    dim_i -= 1
+                dim_i += 1
+            # for the new location, check if this is a new local or global best (if it's valid)
+            particle_error = loss_function(particles_loc[particle_i, :])
+            if particle_error < particles_lowest_loss[particle_i]:  # local best
+                particles_lowest_loss[particle_i] = particle_error
+                particles_best_location[particle_i, :] = particles_loc[
+                    particle_i, :
+                ].copy()
+            if particle_error < global_lowest_loss:  # global best
+                global_lowest_loss = particle_error
+                global_best_location = particles_loc[particle_i, :].copy()
+
+        best_location = global_best_location.copy()
+    return best_location
+
+
+
+
+try:
+    DIM = 9
+    # DIM = 
+    # START = ob.Point(1, 1)
+    # END = ob.Point(10, 20)
+    START = ob.Point(0,0)
+    # END = ob.Point(12.5,15)
+    END = ob.Point(20,8)
+    #test case 1 
+    # obstacle1 = ob.Obstacle_Circle(1.0, ob.Point(2, 6))
+    # obstacle2 = ob.Obstacle_Circle(1.0, ob.Point(5, 1))  
+    # obstacle3 = ob.Obstacle_Circle(1.0, ob.Point(8, 4))
+    # obstacle4 = ob.Obstacle_Circle(1.0, ob.Point(10, 1))#
+    #test case 2
+    obstacle1 = ob.Obstacle_Circle(1.0, ob.Point(6, 8))  
+    obstacle2 = ob.Obstacle_Circle(1.0, ob.Point(10, 4))
+    obstacle3 = ob.Obstacle_Circle(1.0, ob.Point(14, 2))
+    obstacle4 = ob.Obstacle_Circle(1.0, ob.Point(16, 9))
+
+    #test case 3
+
+
+
+    obstacles = [obstacle1, obstacle2, obstacle3,obstacle4]
+
+    best_location = []
+    path = []
+    print("Finding best path with PSO:")
+    print("Loading:")
+    best_location = particle_swarm_optimization(
+        max_iterations=100,
+        swarm_size=200,
+        max_vel=3,
+        step_size=1,
+        inertia=0.9,
+        c1=2.05,
+        c2=2.05,
+        circles=obstacles,
+    )
+    print("100% completed!")
+    ##add start point và end point 
+    # path.append([START.x,START.y])
+
+    for loc in best_location:
+        path.append([loc[0], loc[1]])
+    path.append([END.x,END.y])
+    for i in range(len(path)):
+        path[i][0] /= 10
+        path[i][1] /= 10
+    print("best path:{}".format(path))
+
+    #create socket object
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('192.168.169.84', 12345))
+    server_socket.listen(5)
+    print("Server is listening...")
+    # accept conection from client
+    client_socket, addr = server_socket.accept()
+    print("Connection from {} has been established.".format(addr))
+    #open serial port
+    ser = serial.Serial('/dev/ttyUSB0', 115200)  
+    print("Start Moving!!!!!!!!!!\n")
+    while True:
+        data_received = ser.readline().decode('utf-8').strip()
+        # print(data_received+"\n")
+        #send data to client
+        data_received += "\n"
+        client_socket.sendall(data_received.encode())
+        print("Data sent from server: "+ data_received)
+        cmd,X,Y,Theta = decoder_frame_data(data_received)
+        # cmd,enc_r, enc_l =decoder_frame_enc(data_received)
+        send_point(path,cmd)
+        # print(f"cmd: {cmd}\nx: {X}\ny: {Y}\ntheta: {Theta}\n")
+        # cmd,vr,vl = decoder_frame_vel(data_received);
+        # print("cmd: {}\nvr: {}\nvl: {}\n".format(cmd,vr,vl))
+               
+except KeyboardInterrupt:
+    ser.close()
+    server_socket.close()
